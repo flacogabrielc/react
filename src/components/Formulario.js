f163b8399038aa8a7fe07ab643a5bc4c7d6effe8
Gabriel <flacogabrielc@hotmail.com>
2021-07-01 21:14:24 -0300

    test

---
+++ src/components/Formulario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (  
+        <h1>Desde formulario</h1>
+        //se imprime en app.js llamandolo 
+    );
+}
+ 
+export default Formulario;
